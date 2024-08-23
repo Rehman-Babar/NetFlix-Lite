7d5b2ea198776a508e86951ad95d108603b5da3f
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-08-22 20:54:03 -0700

    404 page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import { Loader } from "lucide-react";
 import WatchPage from "./pages/WatchPage.jsx";
 import SearchPage from "./pages/home/SearchPage.jsx";
 import HistoryPage from "./pages/HistoryPage.jsx"
+import NotFoundPage from "./pages/NotFoundPage.jsx"
 
 
 function App() {
@@ -42,7 +43,7 @@ if (isCheckingAuth) {
       <Route path="/watch/:id" element={user ? <WatchPage/> : <Navigate to={'/login'}/>}/>
       <Route path="/search" element={user ? <SearchPage/> : <Navigate to={'/login'}/>}/>
       <Route path="/history" element={user ? <HistoryPage/> : <Navigate to={'/login'}/>}/>
-      {/* <Route path="/*" element={<NotFoundPage/>}/> */}
+      <Route path="/*" element={<NotFoundPage/>}/>
     </Routes>
     <Footer/>
     <Toaster />
